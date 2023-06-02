9a8a6611ec53725fa7a1883d087b23b223b62567
Evgenij Gotto <egotto@outlook.com>
2023-06-02 23:37:05 +0200

    [File Generator] Start

---
+++ Sorter.BigFiles.Generator/Sorter.BigFiles.Generator/ConfigOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Sorter.BigFiles.Generator
+{
+    public class ConfigOptions
+    {
+        public static string SectionName = "Config";
+
+        public string OutputFilesDirectory { get; set; } = string.Empty;
+        public string InputStingsFilePath { get; set; } = string.Empty;
+        public int OutputFileSizeInMBs { get; set; } = 100;
+    }
+
+}
